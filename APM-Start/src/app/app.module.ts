a4b77f3fb54fa9b084cbc4ea521a690d56a553a2
Lauren Mooney <laurenmooney@nexsystech.com>
2020-10-28 20:26:07 -0400

    finished

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,15 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { ProductModule } from './products/product.module';
 
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './home/welcome.component';
 
 @NgModule({
+  // defines the components/directive/pipes that belong to this module
+  // every one component, directive, and pipe must belong to one, and only one, angular module.
+  // only delcare components, directives, and pipes and never re-declare them if they belong to another module.
+  // all of these are private by default.
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
   ],
+  // allows us to import supporting modules so we can use their exported components, directives, and pipes.
+  // only import modules who's abilities this module needs
+  // importing a module does NOT give you access to that module's imported modules. 
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: 'welcome', component: WelcomeComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' },
+    ]),
+    ProductModule
   ],
-  bootstrap: [AppComponent]
+  // defines the component which is the starting point of the application
+  // every application must bootstrap at least one component, the root application module
+  // the bootstrap array should only be used in the root application module, AppModule
+  bootstrap: [AppComponent] 
 })
 export class AppModule { }
---
+++ APM-Start/src/app/products/product.service.ts
@@ -0,0 +1,37 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import { IProduct } from './product';
+
+@Injectable({
+  providedIn: 'root' // this allows us to access the service from anywhere in the application - with this we no longer need to add the providers array to the module.
+})
+export class ProductService {
+  private productUrl = 'api/products/products.json';
+
+  constructor(private http: HttpClient) { }
+
+  getProducts(): Observable<IProduct[]> {
+    return this.http.get<IProduct[]>(this.productUrl).pipe(
+      tap(data => console.log('All: ' + JSON.stringify(data))),
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(err: HttpErrorResponse): Observable<never> {
+    // in a real world app, we may send the server to some remote logging infrastructure
+    // instead of just logging it to the console
+    let errorMessage = '';
+    if (err.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      errorMessage = `An error occurred: ${err.error.message}`;
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+    }
+    console.error(errorMessage);
+    return throwError(errorMessage);
+  }
+}
